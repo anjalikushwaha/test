f2138c42dd912500b42e909c19cf6a12d2cb17f2
anjalikushwaha <anjalikushwaha475@gmail.com>
2018-06-19 21:20:25 +0530

    first Upload

---
+++ 2.php
@@ -0,0 +1,22 @@
+<?php
+
+$name="anjali";
+$age=24;
+$address="vidya knowledge park,meerut";
+$salary=123.56;
+$subjects=array("DS",2,"c",3,"python",2,"information technology",7);
+$status=true;
+
+print $name;
+echo"<br/>";
+print $age;
+echo"<br/>";
+print $address;
+echo"<br/>";
+print $salary;
+print $subjects[0];
+echo"<br/>";
+print $subjects[1];
+echo"<br/>";
+print $status;
+?>
---
+++ 3.php
@@ -0,0 +1,8 @@
+<?php
+$subjects=array("DS",2,"c",3,"python",2,"information technology",7);
+for($i=0;$i<7;$i++)
+{
+print $subjects[$i];
+echo"<br>";
+}
+?>
